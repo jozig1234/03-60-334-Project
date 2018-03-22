1a3f6db0182c5e9d458cb164d89279aeef4a5efe
jozig1234 <jjourekian@hotmail.com>
2018-03-22 00:29:22 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title> Project </title>
+  <title> Project </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- import bootstrap 4.0.0 then custom styles-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <style>
+  @import url('https://fonts.googleapis.com/css?family=Ubuntu');
+  body{
+    font-family: Ubuntu, sans-serif;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    margin:0px 0px 18px 0px;
+    padding:0px;
+  }
+  .title{
+    font-size:80px;
+    font-weight:800;
+  }
+  </style>
+
 </head>
-<style>
-div.line {
-  line-height: normal;
-}
-</style>
 <body>
-<p><b>Developed by Matthew Morin & Joseph Jourekian</b></p>
-<div style = "width: 100%; padding: 10px; height: 50%; margin: 0 auto; text-align: center; line-height: 0.15">
-   <h1 style = "font-family: Ubuntu, sans-serif; font-weight: bold; font-size: 600%;"> 
-   <span style = "color: #1155cc"> sort by </span>
-   <span style = "color: #ff0000"> views </span></h1>
-   <h5 style = "width = 80%; font-family: Ubuntu, sans-serif; font-size: 150%;">Easily find the stats of any YouTube channel</h5>
-  <div style = "width = 53%; margin: 0 auto; text-align: center; margin-top: 10px;">
-    <form action = "search.php" method = "post">
-    <input name = "channel" type = "text" style = "width: 25%; border-radius: 4px; border: 3px solid #ccc;" placeholder = "channel name here...." size = "55" >
-    <input name = "submit" type = "submit" value = "Begin Search!" size = "15">
-    </form>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-12">
+        <p><b>Developed by Matthew Morin & Joseph Jourekian</b></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12" style="text-align:center;">
+        <h1 class="title"><span style="color: #1155cc">sort by</span><span style="color: #ff0000"> views</span></h1>
+        <h2>Easily find the stats of any YouTube channel</h2>
+        <div style="text-align:center">
+          <form class="form-inline justify-content-center" action="search.php" method="get" style="margin: 0px auto;">
+            <label class="sr-only" for="channel">Channel Name</label>
+            <input name="channel" type="text" class="form-control mb-2 mr-sm-2" id="channel" value="UC27Bqjp8c1cTHcJEnwypkjQ">
+            <button type="submit" class="btn mb-2">Begin Search!</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
   </div>
- </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- search.php
+++ search.php
@@ -1,70 +1,110 @@
 <?php
 //get videos from channel by youtube data API
-$API_key = 'AIzaSyDDq9Qa3La6UZNMkDURslDkbc4hjzMJ0TY';
-$channelID = 'UCh0SxnImUvpWs_ZfEoyhkMA';
-$channelN = $_POST['channel'];
 
+$configs      = include('config.php');
+$API_key      = $configs['youtube_api'];
 
-$maxResults = 10;
-$videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=viewCount&part=snippet&channelId='.$channelID.'&maxResults='.$maxResults.'&key='.$API_key.''));
+$channelID     = $_GET['channel'];
+$maxResults   = 10;
+$youtubeURL = 'https://www.googleapis.com/youtube/v3/search?order=viewCount&part=snippet&channelId='.$channelID.'&maxResults='.$maxResults.'&key='.$API_key.'';
+
+$json = file_get_contents($youtubeURL);
+$videoList = json_decode($json);
+
+// Testing dumps. Lazyyyyy. Don't do this.
+// var_dump($channelID);
+// var_dump($youtubeURL);
+// var_dump($json);
+// var_dump($videoList);
 
 ?>
+
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
 <head>
   <title>Project</title>
-  <meta charset = "utf-8">
-  <meta name = "viewport" content = "width = device-witdh, intiale-scale = 1">
-  <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style type = "text/css">
-  .container {
-    padding: 20px;
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- import bootstrap 4.0.0 then custom styles-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <style>
+  @import url('https://fonts.googleapis.com/css?family=Ubuntu');
+  body{
+    font-family: Ubuntu, sans-serif;
   }
-  .youtube-video h2 {
-    font-size: 16px;
+  h1, h2, h3, h4, h5, h6 {
+    margin:0px 0px 18px 0px;
+    padding:0px;
   }
-  .div {
-    text-align: center;
-    heigth: 100%;
-    margin: 0 auto;
-    border: 2px solid #ccc;
-    border-radius: 4px;
-    background-color: #f8f8f8;
-}
+  <style type="text/css">
+  .navbar-brand{
+    font-weight:800;
+  }
+  .card{
+    margin:20px 0px 0px;
+  }
   </style>
 
 </head>
 <body>
-<div class = "div">
-  <form action = "/search.php" method = "GET" >
-  <a href = "/index.php">
-     <span style ="font-family: Ubuntu, sans-serif; text-align: center; font-weight: bold; font-size:200%">
-     <span style = "color: #1155cc">sort by </span> 
-     <span style = "color: #ff0000">views</span></span>
-  </a> &nbsp
-    <input name = "channel" type = "text" placeholder = "channel name here...." size = "25" style = "padding: 5px; border-radius: 4px; border: 3px solid #ccc;">
-    <input name = "submit" type = "submit" value = "Begin Search!" size = "15">
-  </form>  
-</div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/"><span style="color: #1155cc">sort by </span>
+    <span style="color: #ff0000;">views</span></span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-<div class = "container">
-    <div class = "row">
-    <?php
-      foreach($videoList->items as $item)
-      {
-        if(isset($item->id->videoId))
-        {
-          echo '<div class = "youtube-video">
-              <iframe width="280" height="150" src="https://www.youtube.com/embed/'.$item->id->videoId.'" frameborder="0" allowfullscreen></iframe>
-               
-              <h2>'. $item->snippet->title . '</h2>
-              </div>';  
-        }
-      }
-      ?>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0" action="search.php" method="get">
+        <input name="channel" class="form-control mr-sm-2" type="channel" placeholder="Channel ID" aria-label="channel">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Begin Search!</button>
+      </form>
     </div>
-</div>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+        <?php
+
+        if (!$videoList){
+          ?>
+          <div class="col-sm-12">Please search for a Channel ID.</div>
+          <?php
+        }else {
+
+          $i = 1;
+          foreach($videoList->items as $item)
+          {
+            //var_dump($item);?><br> <?php
+            if(isset($item->id->videoId))
+            { ?>
+                <div class="col-sm-4">
+                  <div class="card">
+                    <img class="card-img-top" src="<?php echo $item->snippet->thumbnails->high->url; ?>" alt="<?php echo $item->snippet->title; ?>">
+                    <div class="card-body">
+                      <h5 class="card-title"><?php echo $i++; echo ": ".$item->snippet->title; ?></h5>
+                      <p class="card-text"><?php echo $item->snippet->description; ?></p>
+                      <a href="https://www.youtube.com/watch?v=<?php echo $item->id->videoId; ?>" class="btn btn-primary">Watch Now!</a>
+                    </div>
+                  </div>
+                </div>
+
+              <?php
+            }
+          }
+        }
+        ?>
+    </div>
+  </div>
+    <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
